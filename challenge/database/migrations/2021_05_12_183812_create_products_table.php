bdc78564f973e5e887805e9ac8e8535bf3355c93
Federico Jacobo <federico@lightit.io>
2021-05-12 15:56:26 -0300

    product and category CRUDs created

---
+++ challenge/database/migrations/2021_05_12_183812_create_products_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('image')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('products_carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('products_id');
+            $table->unsignedBigInteger('carts_id');
+            $table->timestamps();
+
+            $table->unique(['products_id', 'carts_id']);
+
+            $table->foreign('products_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+
+            $table->foreign('carts_id')
+                ->references('id')
+                ->on('carts')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('products_carts');
+    }
+}
